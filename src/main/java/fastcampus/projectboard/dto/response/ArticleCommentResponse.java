e9c23d69583f5887bc6652ba48b933ea45886a82
Hyeyeon Jo <johy1219@naver.com>
2022-07-27 00:24:20 +0900

    #18 - 서비스 로직 테스트 코드 업데이트 및 DTO재설계, 쿼리 메소드 작성
    나머지 로직의 테스트 코드를 작성하고
    연관된 DTO를 재설게함
    테스트가 필요로하는 repository 쿼리 메소드도 작성

---
+++ src/main/java/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
@@ -0,0 +1,35 @@
+package fastcampus.projectboard.dto.response;
+
+import fastcampus.projectboard.dto.ArticleCommentDto;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class ArticleCommentResponse implements Serializable {
+    private final LocalDateTime createdAt;
+    private final Long id;
+    private final String content;
+    private final String email;
+    private final String nickname;
+
+    public static ArticleCommentResponse of(LocalDateTime createdAt, Long id, String content, String email, String nickname) {
+        return new ArticleCommentResponse(createdAt, id, content, email, nickname);
+    }
+
+    public static ArticleCommentResponse from(ArticleCommentDto dto) {
+        String nickname = dto.getUserAccountDto().getNickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.getUserAccountDto().getUserId();
+        }
+
+        return new ArticleCommentResponse(
+                dto.getCreatedAt(),
+                dto.getId(),
+                dto.getContent(),
+                dto.getUserAccountDto().getEmail(),
+                nickname
+        );
+    }
+}
---
+++ src/main/java/fastcampus/projectboard/dto/response/ArticleWithCommentResponse.java
@@ -0,0 +1,50 @@
+package fastcampus.projectboard.dto.response;
+
+import fastcampus.projectboard.domain.Article;
+import fastcampus.projectboard.dto.ArticleCommentDto;
+import fastcampus.projectboard.dto.ArticleWithCommentsDto;
+import fastcampus.projectboard.dto.UserAccountDto;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+public class ArticleWithCommentResponse implements Serializable {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String hashtag;
+    private final LocalDateTime createdAt;
+    private final String email;
+    private final String nickname;
+    private final Set<ArticleCommentResponse> articleCommentResponses;
+
+    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+    }
+
+    public static ArticleWithCommentResponse from(ArticleWithCommentsDto dto) {
+        String nickname = dto.getUserAccountDto().getNickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.getUserAccountDto().getUserId();
+        }
+
+        return new ArticleWithCommentResponse(
+                dto.getId(),
+                dto.getTitle(),
+                dto.getContent(),
+                dto.getHashtag(),
+                dto.getCreatedAt(),
+                dto.getUserAccountDto().getEmail(),
+                nickname,
+                dto.getArticleCommentDtos().stream()
+                        .map(ArticleCommentResponse::from)
+                        .collect(Collectors.toCollection(LinkedHashSet::new))
+        );
+    }
+}
